6e31d32f7415451454f49f600a862315ba767ee3
nscc-asp2a <e1083772@u.nus.edu>
2024-10-16 16:06:06 +0800

    min max scaling of RNA-Seq

--- utils/scaler.py
+++ utils/scaler.py
@@ -4,22 +4,39 @@ import pandas as pd
 # scale and impute train examples
 def scale_and_impute_without_train_test_leak(train_dataframe, valid_dataframe):
 
-    train_dataframe_standardscale_cols = train_dataframe.filter(regex='^(?:Feature_exp|Feature_clin_D_PT_age)')
-    train_dataframe_noneedscale_cols = train_dataframe.filter(regex='^(?!Feature_exp|Feature_clin_D_PT_age)')
+    # Separate scaling for Feature_exp and Feature_clin_D_PT_age
+    train_feature_exp_cols = train_dataframe.filter(regex='^Feature_exp')
+    train_feature_clin_age_cols = train_dataframe.filter(regex='^Feature_clin_D_PT_age')
+    train_noneedscale_cols = train_dataframe.filter(regex='^(?!Feature_exp|Feature_clin_D_PT_age)')
     
-    valid_dataframe_standardscale_cols = valid_dataframe.filter(regex='^(?:Feature_exp|Feature_clin_D_PT_age)')
-    valid_dataframe_noneedscale_cols = valid_dataframe.filter(regex='^(?!Feature_exp|Feature_clin_D_PT_age)')
+    valid_feature_exp_cols = valid_dataframe.filter(regex='^Feature_exp')
+    valid_feature_clin_age_cols = valid_dataframe.filter(regex='^Feature_clin_D_PT_age')
+    valid_noneedscale_cols = valid_dataframe.filter(regex='^(?!Feature_exp|Feature_clin_D_PT_age)')
 
-    train_means = np.mean(train_dataframe_standardscale_cols, axis=0)
-    train_stds = np.std(train_dataframe_standardscale_cols, axis=0)
+    # 0-1 Min-Max scaling
+    train_feature_exp_cols_scaled = train_feature_exp_cols
+    valid_feature_exp_cols_scaled = valid_feature_exp_cols
+    train_feature_exp_min = train_feature_exp_cols_scaled.min(axis=0)
+    train_feature_exp_max = train_feature_exp_cols_scaled.max(axis=0)
+    train_feature_exp_cols_scaled = (train_feature_exp_cols_scaled - train_feature_exp_min) / (train_feature_exp_max - train_feature_exp_min)
+    valid_feature_exp_cols_scaled = (valid_feature_exp_cols_scaled - train_feature_exp_min) / (train_feature_exp_max - train_feature_exp_min)
+
+    # Clipping valid columns to be within 0 and 1
+    valid_feature_exp_cols_scaled = valid_feature_exp_cols_scaled.clip(0, 1)
     
-    train_dataframe_standardscale_cols_scaled = (train_dataframe_standardscale_cols - train_means) / train_stds
-    valid_dataframe_standardscale_cols_scaled = (valid_dataframe_standardscale_cols - train_means) / train_stds
-    
-    train_dataframe_scaled = pd.concat([train_dataframe_noneedscale_cols, 
-                                        train_dataframe_standardscale_cols_scaled], axis=1)
-    valid_dataframe_scaled = pd.concat([valid_dataframe_noneedscale_cols, 
-                                        valid_dataframe_standardscale_cols_scaled],axis=1)
+    # Scaling Feature_clin_D_PT_age columns
+    train_feature_clin_age_means = np.mean(train_feature_clin_age_cols, axis=0)
+    train_feature_clin_age_stds = np.std(train_feature_clin_age_cols, axis=0)
+    train_feature_clin_age_cols_scaled = (train_feature_clin_age_cols - train_feature_clin_age_means) / train_feature_clin_age_stds
+    valid_feature_clin_age_cols_scaled = (valid_feature_clin_age_cols - train_feature_clin_age_means) / train_feature_clin_age_stds
+
+    # Concatenate all scaled and non-scaled columns
+    train_dataframe_scaled = pd.concat([train_noneedscale_cols, 
+                                        train_feature_exp_cols_scaled, 
+                                        train_feature_clin_age_cols_scaled], axis=1)
+    valid_dataframe_scaled = pd.concat([valid_noneedscale_cols, 
+                                        valid_feature_exp_cols_scaled, 
+                                        valid_feature_clin_age_cols_scaled], axis=1)
 
     train_dataframe_scaled_fillna = train_dataframe_scaled.fillna(0)
     # no need fo fillna in validation dataframe because there are no NAs
