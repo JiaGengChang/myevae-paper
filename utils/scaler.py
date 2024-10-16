fae942ba0cb554905ade4b848de6823eb5d0964e
jiageng <jiageng1997@gmail.com>
2024-10-17 01:09:24 +0800

    min-max scaling for all features

--- utils/scaler.py
+++ utils/scaler.py
@@ -4,42 +4,28 @@ import pandas as pd
 # scale and impute train examples
 def scale_and_impute_without_train_test_leak(train_dataframe, valid_dataframe):
 
-    # Separate scaling for Feature_exp and Feature_clin_D_PT_age
-    train_feature_exp_cols = train_dataframe.filter(regex='^Feature_exp')
-    train_feature_clin_age_cols = train_dataframe.filter(regex='^Feature_clin_D_PT_age')
-    train_noneedscale_cols = train_dataframe.filter(regex='^(?!Feature_exp|Feature_clin_D_PT_age)')
+    # pull out features
+    train_features = train_dataframe.filter(regex='^(?:Feature_)')
+    valid_features = valid_dataframe.filter(regex='^(?:Feature_)')
     
-    valid_feature_exp_cols = valid_dataframe.filter(regex='^Feature_exp')
-    valid_feature_clin_age_cols = valid_dataframe.filter(regex='^Feature_clin_D_PT_age')
-    valid_noneedscale_cols = valid_dataframe.filter(regex='^(?!Feature_exp|Feature_clin_D_PT_age)')
-
-    # 0-1 Min-Max scaling
-    train_feature_exp_cols_scaled = train_feature_exp_cols
-    valid_feature_exp_cols_scaled = valid_feature_exp_cols
-    train_feature_exp_min = train_feature_exp_cols_scaled.min(axis=0)
-    train_feature_exp_max = train_feature_exp_cols_scaled.max(axis=0)
-    train_feature_exp_cols_scaled = (train_feature_exp_cols_scaled - train_feature_exp_min) / (train_feature_exp_max - train_feature_exp_min)
-    valid_feature_exp_cols_scaled = (valid_feature_exp_cols_scaled - train_feature_exp_min) / (train_feature_exp_max - train_feature_exp_min)
+    # 0-1 Min-Max scaling for everything
+    train_feature_min = train_features.min(axis=0)
+    train_feature_max = train_features.max(axis=0)
+    train_features_scaled = (train_features - train_feature_min) / (train_feature_max - train_feature_min)
+    valid_features_scaled = (valid_features - train_feature_min) / (train_feature_max - train_feature_min)
 
     # Clipping valid columns to be within 0 and 1
-    valid_feature_exp_cols_scaled = valid_feature_exp_cols_scaled.clip(0, 1)
-    
-    # Scaling Feature_clin_D_PT_age columns
-    train_feature_clin_age_means = np.mean(train_feature_clin_age_cols, axis=0)
-    train_feature_clin_age_stds = np.std(train_feature_clin_age_cols, axis=0)
-    train_feature_clin_age_cols_scaled = (train_feature_clin_age_cols - train_feature_clin_age_means) / train_feature_clin_age_stds
-    valid_feature_clin_age_cols_scaled = (valid_feature_clin_age_cols - train_feature_clin_age_means) / train_feature_clin_age_stds
+    valid_features_scaled = valid_features_scaled.clip(0, 1)
 
-    # Concatenate all scaled and non-scaled columns
-    train_dataframe_scaled = pd.concat([train_noneedscale_cols, 
-                                        train_feature_exp_cols_scaled, 
-                                        train_feature_clin_age_cols_scaled], axis=1)
-    valid_dataframe_scaled = pd.concat([valid_noneedscale_cols, 
-                                        valid_feature_exp_cols_scaled, 
-                                        valid_feature_clin_age_cols_scaled], axis=1)
-
-    train_dataframe_scaled_fillna = train_dataframe_scaled.fillna(0)
-    # no need fo fillna in validation dataframe because there are no NAs
-    assert valid_dataframe_scaled.isna().sum().sum() == 0, "validation dataframe has NaNs"
+    # Filling NAs in training data with 0
+    train_features_scaled = train_features_scaled.fillna(0)
     
-    return train_dataframe_scaled_fillna, valid_dataframe_scaled
+    # valid fillna is only required for shuffle 5 fold 3
+    # where RNA-Seq feature ENSG00000212725 has train_min = train_max
+    valid_features_scaled = valid_features_scaled.fillna(0)
+    
+    # add back the survival columns
+    train_dataframe_scaled = pd.concat([train_dataframe[['survtime','survflag']], train_features_scaled], axis=1)
+    valid_dataframe_scaled = pd.concat([valid_dataframe[['survtime','survflag']], valid_features_scaled], axis=1)
+    
+    return train_dataframe_scaled, valid_dataframe_scaled
