b891586d9fb582740b26e09fc2a19d1d13df41c4
jiageng <jiageng1997@gmail.com>
2024-10-17 02:15:40 +0800

    correct path for .env file

--- utils/parsers.py
+++ utils/parsers.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 from dotenv import load_dotenv
-load_dotenv('.env')
+load_dotenv('../.env')
 
 def parse_surv(endpoint): # pfs or os
     surv=pd.read_csv(os.environ.get("SURVDATAFILE"),sep='\t')\
