acfdfd52d1c119818f2c93ca4efd34f8635398eb
jiageng <jiageng1997@gmail.com>
2024-11-20 14:22:03 +0800

    UMAP of latent embeddings

---
+++ modules_vae/latent.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+import umap
+
+wdir='/home/users/nus/e1083772/cancer-survival-ml/vae_models'
+exptname='RNA'
+endpoint='os'
+shuffle='0'
+fold='4'
+
+files = glob(f'{wdir}/{exptname}/{endpoint}_shuffle{shuffle}_fold{fold}.tsv')
+plt.clf()
+
+for f in files:
+    mu = pd.read_csv(f,sep='\t')
+
+    # Select the columns mu_0 to mu_127
+    mu_features = mu[[f'mu_{i}' for i in range(128)]]
+
+    # Fit and transform the data using UMAP
+    reducer = umap.UMAP(n_components=2, random_state=42)
+    embedding = reducer.fit_transform(mu_features)
+
+    # Add the UMAP components to the dataframe
+    mu['umap_0'] = embedding[:, 0]
+    mu['umap_1'] = embedding[:, 1]
+
+    plt.scatter(mu['umap_0'], mu['umap_1'], c=mu['prediction'], cmap='viridis')
+    
+plt.xlabel('UMAP 1')
+plt.ylabel('UMAP 2')
+plt.colorbar(label='Prediction')
+plt.title(f'{exptname} {endpoint.upper()} Shuffle {shuffle} Fold {fold}')
+plt.show()
+plt.savefig(f'{wdir}/{exptname}/UMAP-{endpoint}-{shuffle}-{fold}.png')
