e7d50d4ab64d5fdd1016d35dab3c957fb508d2b0
jiageng <jiageng1997@gmail.com>
2024-11-20 14:21:39 +0800

    option to save 'mu'

--- modules_vae/predict.py
+++ modules_vae/predict.py
@@ -2,21 +2,30 @@ import torch
 import numpy as np
 import pandas as pd
 
-def predict_to_tsv(model, loader, outputfile):
+def predict_to_tsv(model, loader, outputfile, save_embeddings=False):
     model.eval()
     predictions = []
     public_ids = []
+    if save_embeddings:
+        mu_list = []
     for batch_idx, data in enumerate(loader):
         with torch.no_grad():
             inputs_vae = [data[f'X_{input_type}'] for input_type in model.input_types_vae]
             inputs_task = [data[f'X_{input_type}'] for input_type in model.input_types_subtask]
-            _, _, _, riskpred = model.forward((inputs_vae, inputs_task))
+            _, mu, _, riskpred = model.forward((inputs_vae, inputs_task))
             predictions.append(riskpred.detach().numpy())
+            if save_embeddings:
+                mu_list.append(mu.detach().numpy())
             public_ids.append(data['PUBLIC_ID'])
     predictions = np.concatenate(predictions)
     public_ids = np.concatenate(public_ids)
+    if save_embeddings:
+        mu_list = np.concatenate(mu_list)
     df = pd.DataFrame({
         'PUBLIC_ID': public_ids,
         'prediction': predictions.flatten()
     })
+    if save_embeddings:
+        mu_columns = [f'mu_{i}' for i in range(mu_list.shape[1])]
+        df[mu_columns] = pd.DataFrame(mu_list)
     df.to_csv(outputfile, index=False, sep='\t')
