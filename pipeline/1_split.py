d476deabc66fcce3dca51a3278b023eb3f3ee227
jiageng <jiageng1997@gmail.com>
2025-04-07 02:06:17 +0800

    aux function to create full dataset

--- pipeline/1_split.py
+++ pipeline/1_split.py
@@ -1,29 +1,20 @@
 import os
-os.chdir('/home/users/nus/e1083772/cancer-survival-ml/')
-import argparse
-import pandas as pd
-import numpy as np
-
+from dotenv import load_dotenv
+assert load_dotenv('../.env') or load_dotenv('.env')
 import sys
-sys.path.append('/home/users/nus/e1083772/cancer-survival-ml/')
-from utils.dataset import Dataset
+sys.path.append(os.environ.get("PROJECTDIR"))
 from utils.parsers import parse_all
 from utils.splitter import kfold_split
 
-def main():
-    REDO=True # whether to overwrite existing parquet files
-    
-    _pbs_array_id = int(os.getenv('PBS_ARRAY_INDEX', "-1"))
-    shuffle=_pbs_array_id%10
-    fold=_pbs_array_id//10
+def main(REDO:bool,shuffle:int,fold:int) -> None:
 
     # begin writing split data
-    scratchdir="/scratch/users/nus/e1083772/cancer-survival-ml/data/splits"
-    train_features_file=f'{scratchdir}/{shuffle}/{fold}/train_features.parquet'
-    valid_features_file=f'{scratchdir}/{shuffle}/{fold}/valid_features.parquet'
+    splitsdir=os.environ.get("SPLITDATADIR")
+    train_features_file=f'{splitsdir}/{shuffle}/{fold}/train_features.parquet'
+    valid_features_file=f'{splitsdir}/{shuffle}/{fold}/valid_features.parquet'
 
-    train_labels_file=f'{scratchdir}/{shuffle}/{fold}/train_labels.parquet'
-    valid_labels_file=f'{scratchdir}/{shuffle}/{fold}/valid_labels.parquet'
+    train_labels_file=f'{splitsdir}/{shuffle}/{fold}/train_labels.parquet'
+    valid_labels_file=f'{splitsdir}/{shuffle}/{fold}/valid_labels.parquet'
 
     if not os.path.isfile(train_features_file) \
         or not os.path.isfile(valid_features_file) \
@@ -51,6 +42,30 @@ def main():
     
     return
 
+def aux()->None:
+    # creates a full dataset rather than split the data
+    # useful for external validation
+    # shuffle and fold are ignored
+    full_dataframe = parse_all()
+    survcols=['oscdy','censos','pfscdy','censpfs']
+    full_surv = full_dataframe.loc[:,survcols]
+    full_dataframe.drop(columns=survcols,inplace=True)
+    splitsdir=os.environ.get("SPLITDATADIR")
+    full_features_file=f'{splitsdir}/full_features.parquet'
+    full_labels_file=f'{splitsdir}/full_labels.parquet'
+    if not os.path.exists(os.path.dirname(full_features_file)):
+        os.makedirs(os.path.dirname(full_features_file), exist_ok=True)
+    full_dataframe.to_parquet(full_features_file,compression=None)
+    full_surv.to_parquet(full_labels_file, compression=None)
+
+    return
 
 if __name__ == "__main__":
-    main()
+    REDO=True # overwrite outputs
+    _pbs_array_id = int(os.getenv('PBS_ARRAY_INDEX', "-1"))
+    pbs_shuffle=_pbs_array_id%10
+    pbs_fold=_pbs_array_id//10
+
+    main(REDO, pbs_shuffle, pbs_fold)
+    # if REDO:
+    #     aux()
