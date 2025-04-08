af973391a3eef613a91f7474f29789070f8db2fd
jiageng <jiageng1997@gmail.com>
2025-04-09 01:36:15 +0800

    accepts single df only, return gene list as well

--- utils/subset_affy_features.py
+++ utils/subset_affy_features.py
@@ -10,28 +10,27 @@ assert load_dotenv('../.env') or load_dotenv('..env')
 # Hugene 1.0 ST V1
 # this is to reduce the number of NaN features in gene expression only models
 # hopefully improving performance on external validation datasets
-def subset_to_microarray_genes(train_features:pd.DataFrame,valid_features:pd.DataFrame=None) -> tuple[pd.DataFrame]:
+def subset_to_microarray_genes(df:pd.DataFrame) -> tuple[pd.DataFrame,list[str]]:
     
     # gene IDs with affymetrix probes
     affygenes = np.loadtxt(os.environ.get('AFFYGENESFILE'),dtype=str).tolist()
     
     # names of gene IDs used as features
-    genefeatures = train_features.filter(regex='Feature_exp').columns.str.extract('^.*(ENSG.*)$')
+    genenames = df.filter(regex='Feature_exp').columns.str.extract('^.*(ENSG.*)$')
     
     # indices of gene features that do not have affy probes
-    idx_not_in_affy = np.where(~genefeatures.iloc[:,0].isin(affygenes))
+    idx_not_in_affy = np.where(~genenames.iloc[:,0].isin(affygenes))
     
     # names of gene features that do not have affy probes
-    train_features_not_in_afffy = train_features.filter(regex='Feature_exp').columns[idx_not_in_affy]
+    colnames_not_in_affy = df.filter(regex='Feature_exp').columns[idx_not_in_affy]
+    
+    # set aside genes in affy probes to return
+    affy_genenames = list(set(affygenes).intersection(set(genenames.iloc[:,0])))
     
     # all features except the gene features without affy probes
-    features_keep = train_features.columns[~train_features.columns.isin(train_features_not_in_afffy)]
+    features_keep = df.columns[~df.columns.isin(colnames_not_in_affy)]
     
     # subset the feature columns
-    train_features_subset = train_features[features_keep]
+    df_keep = df[features_keep]
     
-    if valid_features is not None:
-        valid_features_subset = valid_features[features_keep]
-        return train_features_subset, valid_features_subset
-    else:
-        return train_features_subset, None
+    return df_keep, affy_genenames
