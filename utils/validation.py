560d2f07480dc9248790ea26d1651387edca7a15
jiageng <jiageng1997@gmail.com>
2025-04-09 01:37:01 +0800

    model parameter has to be torch nn module

--- utils/validation.py
+++ utils/validation.py
@@ -19,42 +19,33 @@ def score_external_datasets(model:Module,params:dict,level:str="affy")->tuple[fl
     @Outputs:
         a tuple of C-indexes of the model evaluated on the UAMS, HOVON, and EMTAB microarray datasets
     """
-    # the only permitted input types for external validation
+    # the only permitted input types for externßal validation
     assert params.input_types_all == ['exp', 'clin']
 
     # subset external validation data to genes seen by the model
-    try:
-        genes = params.genes
-    except AttributeError:
-        # Asusme in this case we train the model on all 700+ genes 
-        # regardless of whether they exist in microarray
-        # genes = helper_get_training_genes(params.endpoint,params.shuffle,params.fold)
-        # update: this is handled in an earlier step and stored as params.exp_genes
-        genes = params.exp_genes 
-
     uams_clin_tensor = torch_tensor(scale_impute(parse_clin_uams(), params.scale_method).values)
-    uams_exp_tensor = torch_tensor(scale_impute(parse_exp_uams(genes,level), params.scale_method).values)
+    uams_exp_tensor = torch_tensor(scale_impute(parse_exp_uams(params.genes,level), params.scale_method).values)
 
     hovon_clin_tensor = torch_tensor(scale_impute(parse_clin_hovon(), params.scale_method).values)
-    hovon_exp_tensor = torch_tensor(scale_impute(parse_exp_hovon(genes,level), params.scale_method).values)
+    hovon_exp_tensor = torch_tensor(scale_impute(parse_exp_hovon(params.genes,level), params.scale_method).values)
 
     emtab_clin_tensor = torch_tensor(scale_impute(parse_clin_emtab(), params.scale_method).values)
-    emtab_exp_tensor = torch_tensor(scale_impute(parse_exp_emtab(genes,level), params.scale_method).values)
+    emtab_exp_tensor = torch_tensor(scale_impute(parse_exp_emtab(params.genes,level), params.scale_method).values)
     
     uams_events, uams_times = parse_surv_uams(params.endpoint)
     hovon_events, hovon_times = parse_surv_hovon(params.endpoint)
     emtab_events, emtab_times = parse_surv_emtab(params.endpoint)
 
     if params.architecture=="VAE":
-        # use the VAE API for scoring
+        # use the VAE API for scoring. VAE is a torch.nn.Module
         model.eval()
         with no_grad():
             _, _, _, estimates_uams =  model([[uams_exp_tensor], [uams_clin_tensor]])
             _, _, _, estimates_hovon = model([[hovon_exp_tensor], [hovon_clin_tensor]])
             _, _, _, estimates_emtab = model([[emtab_exp_tensor], [emtab_clin_tensor]])
     elif params.architecture=='Deepsurv':
-        model._model.eval()
-        # use the Deepsurv API for scoring
+        model.eval()
+        # use the Deepsurv API for scoring. model is a CoxPH object (pycox)
         with no_grad():
             # potential bug, I don't know if -1 is the concatenation axis
             estimates_uams =  model(torch_cat([uams_exp_tensor, uams_clin_tensor],axis=-1))
