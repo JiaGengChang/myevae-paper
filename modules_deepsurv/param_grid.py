f0bb130518a61feed4113b1a256e43f98e923b4d
jiageng <jiageng1997@gmail.com>
2025-04-09 11:51:59 +0800

    more parameters

--- modules_deepsurv/param_grid.py
+++ modules_deepsurv/param_grid.py
@@ -1,9 +1,15 @@
 from torch.nn.activations import LeakyReLU, ReLU, Sigmoid
 
 param_grid = {
+    'input_types_all':[['exp','clin']],
     'lr': [5e-4, 1e-4, 5e-5, 1e-5],
-    'layer_dims': [[None, 32, 1], [None, 64, 1], [None, 128, 32, 1], [None, 512, 128, 32, 1]],
+    'layer_dims': [[32, 1], [64, 1], [128, 32, 1]],
     'batch_size': [128, 256, 512],
     'dropout': [0.2, 0.3, 0.5],
     'activation': [LeakyReLU(), ReLU(), Sigmoid()],
+    'epochs': [300],
+    'burn_in': [50],
+    'patience': [20],
+    'subset_microarray': [True, False],
+    'scale_method': ['std']
 }
