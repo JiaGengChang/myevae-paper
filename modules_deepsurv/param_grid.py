7f26f2ea0a1bf8be7e0921adfafb39661d63b0c4
jiageng <jiageng1997@gmail.com>
2025-04-10 22:05:23 +0800

    latest param grid

--- modules_deepsurv/param_grid.py
+++ modules_deepsurv/param_grid.py
@@ -1,15 +1,30 @@
-from torch.nn.activations import LeakyReLU, ReLU, Sigmoid
+from torch.nn import LeakyReLU, ReLU, Sigmoid
 
 param_grid = {
-    'input_types_all':[['exp','clin']],
+    'input_types_all':[['exp','cna','clin']],
     'lr': [5e-4, 1e-4, 5e-5, 1e-5],
-    'layer_dims': [[32, 1], [64, 1], [128, 32, 1]],
+    'layer_dims': [[64, 16, 1], [128, 32, 1], [256, 64, 8, 1]],
     'batch_size': [128, 256, 512],
     'dropout': [0.2, 0.3, 0.5],
     'activation': [LeakyReLU(), ReLU(), Sigmoid()],
     'epochs': [300],
     'burn_in': [50],
     'patience': [20],
-    'subset_microarray': [True, False],
+    'subset_microarray': [False], # want to do this only for full models
+    'scale_method': ['std'] # only for external validation with full model
+}
+
+# for debugging fit_gridsearchcv.py
+param_grid_debug = {
+    'input_types_all':[['exp','clin']],
+    'layer_dims': [[2, 1], [4, 1]],
+    'lr': [5e-4],
+    'batch_size': [1024],
+    'dropout': [0.5],
+    'activation': [LeakyReLU()],
+    'epochs': [100],
+    'burn_in': [10],
+    'patience': [10],
+    'subset_microarray': [False],
     'scale_method': ['std']
 }
--- modules_vae/param_grid.py
+++ modules_vae/param_grid.py
@@ -1,7 +1,38 @@
+from torch.nn import LeakyReLU, ReLU, Sigmoid, Tanh
+
 param_grid = {
     'z_dim': [8, 16, 32],
     'lr': [5e-4, 1e-4, 5e-5], 
-    'batch_size': [32, 64, 128],
-    'layer_dims': [[[32]], [[64]], [[128]]],
-    'layer_dims_subtask' : [[4,1], [8,1], [16,1]]
+    'batch_size': [128, 256, 512],
+    'input_types': [['exp','cna']],
+    'input_types_subtask': [['clin']],
+    'layer_dims': [[[64, 16], [16, 8]], [[128, 32], [64, 16]], [[256, 64], [128, 32]]],
+    'layer_dims_subtask' : [[4,1], [8,1], [16,1]],
+    'kl_weight': [1],
+    'activation': [LeakyReLU(),ReLU(),Sigmoid()],
+    'subtask_activation': [Tanh(), Sigmoid()],
+    'epochs': [300],
+    'burn_in': [50],
+    'patience': [20],
+    'subset_microarray': [False], # want to do this only for full models
+    'scale_method': ['std'] # only for external validation with full model
+}
+
+# for debugging uses
+param_grid_debug = {
+    'z_dim': [8],
+    'lr': [5e-4], 
+    'batch_size': [1024],
+    'input_types': [['exp']],
+    'input_types_subtask': [['clin']],
+    'layer_dims': [[[32, 4]]],
+    'layer_dims_subtask' : [[4,1]],
+    'kl_weight': [1],
+    'activation': [LeakyReLU()],
+    'subtask_activation': [Tanh()],
+    'epochs': [100],
+    'burn_in': [20],
+    'patience': [5],
+    'subset_microarray': [True, False], # want to do this only for full models
+    'scale_method': ['std'] # only for external validation with full model
 }
