f0e999e713b9af82079789491da015d184a4788e
jiageng <jiageng1997@gmail.com>
2025-04-10 22:05:02 +0800

    minor refactor

--- pipeline/3_fit_gridsearchcv.py
+++ pipeline/3_fit_gridsearchcv.py
@@ -10,12 +10,10 @@ import sys
 sys.path.append(os.environ.get("PROJECTDIR"))
 from modules_vae.estimator import VAE
 from modules_deepsurv.estimator import DeepSurv
-from modules_vae.params import VAEParams
-from modules_deepsurv.params import DeepsurvParams
-from modules_vae.param_grid import param_grid
+from modules_vae.param_grid import param_grid as param_grid_vae
+from modules_deepsurv.param_grid import param_grid as param_grid_deepsurv
+from utils.params import VAEParams, DeepsurvParams
 from utils.validation import score_external_datasets
-from utils.subset_affy_features import subset_to_microarray_genes
-from utils.lazy_input_dims import lazy_input_dims
 from utils.annotate_exp_genes import annotate_exp_genes
 
 import joblib
@@ -26,12 +24,14 @@ from dask.distributed import Client, LocalCluster
 # if model is exp-only, perform benchmark on external GEO datasets
 # scores on these external dataset will also be in the json file
 # select the model to be either VAE (default) or Deepsurv (benchmark)
-def main(endpoint:str='os', shuffle:int=5, fold:int=10, architecture:str='VAE', fulldata:bool=False) -> None:
+def main(model_name:str='default', endpoint:str='os', shuffle:int=5, fold:int=10, architecture:str='VAE', fulldata:bool=False) -> None:
     
-    if architecture=='Deepsurv':
-        params = DeepsurvParams(endpoint=endpoint, shuffle=shuffle, fold=fold, fulldata=fulldata)
-    elif architecture=='VAE':
-        params = VAEParams(endpoint=endpoint, shuffle=shuffle, fold=fold, fulldata=fulldata)
+    if architecture=='VAE':
+        params = VAEParams(model_name=model_name, endpoint=endpoint, shuffle=shuffle, fold=fold, fulldata=fulldata)
+        param_grid = param_grid_vae
+    elif architecture=='Deepsurv':
+        params = DeepsurvParams(model_name=model_name,endpoint=endpoint, shuffle=shuffle, fold=fold, fulldata=fulldata)
+        param_grid = param_grid_deepsurv
     else:
         raise NotImplementedError(architecture)
     
@@ -46,11 +46,7 @@ def main(endpoint:str='os', shuffle:int=5, fold:int=10, architecture:str='VAE', 
         assert os.path.exists(train_features_file) and os.path.exists(train_labels_file)
         train_features=pd.read_parquet(train_features_file)
         train_labels=pd.read_parquet(train_labels_file)[[params.eventcol,params.durationcol]]
-        # sets params.exp_genes to all RNA genes in train_features
         params = annotate_exp_genes(train_features, params)
-        # should genes be subset to microarray genes ?
-        if params.subset_microarray:
-            train_features,_ = subset_to_microarray_genes(train_features)
         train_dataframe=pd.concat([train_labels,train_features],axis=1)
     else:
         # the default mode
@@ -66,19 +62,15 @@ def main(endpoint:str='os', shuffle:int=5, fold:int=10, architecture:str='VAE', 
         train_labels=pd.read_parquet(train_labels_file)[[params.eventcol,params.durationcol]]
         valid_features=pd.read_parquet(valid_features_file)
         valid_labels=pd.read_parquet(valid_labels_file)[[params.eventcol,params.durationcol]]
-        # sets params.exp_genes to all RNA genes in train_features
         params = annotate_exp_genes(train_features, params)
-        # should genes be subset to microarray genes ?
-        if params.subset_microarray:
-            train_features,valid_features = subset_to_microarray_genes(train_features,valid_features)
         train_dataframe=pd.concat([train_labels,train_features],axis=1)
         valid_dataframe=pd.concat([valid_labels,valid_features],axis=1)
 
-    params = lazy_input_dims(train_dataframe, params) # determine input dimensions. params.input_dims
-    
     if architecture=='VAE':
+        # lazy input dims and subtask input dims is already built into VAE estimator.py
         base_estimator = VAE(eventcol=params.eventcol,durationcol=params.durationcol)
     elif architecture=='Deepsurv':
+        # lazy input dims is already built into Deepsurv
         base_estimator = DeepSurv(eventcol=params.eventcol,durationcol=params.durationcol)
     else:
         raise NotImplementedError(architecture)
@@ -87,45 +79,76 @@ def main(endpoint:str='os', shuffle:int=5, fold:int=10, architecture:str='VAE', 
 
     cluster = LocalCluster()
     client = Client(cluster)
-    with joblib.parallel_config("dask", n_jobs=4):
+    with joblib.parallel_config("dask", n_jobs=12): # set n_jobs to NCPUS
         grid_search.fit(train_dataframe)
 
-    valid_metric = 0 if fulldata else grid_search.score(valid_dataframe)
+    # update params with best params
+    for k,v in grid_search.best_params_.items():
+        setattr(params,k,v)
+    # update params with RNA-Seq gene names
+    # this field is needed in score_external_datasets
+    if grid_search.best_estimator_.subset_microarray:
+        # only microarray compatible genes were used
+        setattr(params,"genes",grid_search.best_estimator_.genes)
+    else:
+        # all genes were used, including those not found in microarray
+        setattr(params,"genes",params.all_exp_genes)
+        
     results = {}
-    results['params_fixed'] = {k: v for k, v in vars(params).items() if not k.startswith('_') and k != 'exp_genes' and k not in param_grid.keys()}
-    results['params_search'] = param_grid
-    results['best_model'] = {}
-    results['best_model']['params'] = grid_search.best_params_
-    results['best_model']['valid_metric'] = valid_metric
+    # drop exp genes because it is too long
+    # but do print out the final genes used
+    results['params_fixed'] = {k: v for k, v in vars(params).items() if not k.startswith('_') and k != 'all_exp_genes' and k not in param_grid.keys()}
+    results['params_search'] = {k: v.__str__() for k, v in param_grid.items() } # save activation as string
+    results['best_epoch'] = {}
+    results['best_epoch']['params'] = {k:v.__str__() for k, v in grid_search.best_params_.items()} # save activation as string
+
+    # skip if this model is for external validation
+    if params.fulldata:
+        results['best_epoch']['valid_metric'] = 0
+    else:
+        valid_metric = grid_search.score(valid_dataframe)
+        results['best_epoch']['valid_metric'] = valid_metric
 
     # score external datasets if using only RNASeq as input
-    if params.input_types_all == ['exp','clin']:
+    if grid_search.best_estimator_.input_types_all==['exp','clin']:
+        # pass in the torch.nn.Module
         try:
-            params.exp_genes # genes seen by the model. Not necessarily all input genes.
+            best_estimator = grid_search.best_estimator_.model.net # DeepSurv estimator
         except AttributeError:
-            params.exp_genes=None
-        
-        cindex_uams, cindex_hovon, cindex_emtab = score_external_datasets(grid_search.best_estimator_.model, params)
-        results['best_model']['uams_metric'] = cindex_uams
-        results['best_model']['hovon_metric'] = cindex_hovon
-        results['best_model']['emtab_metric'] = cindex_emtab
+            best_estimator = grid_search.best_estimator_.model # VAE estimator
+            
+        cindex_uams, cindex_hovon, cindex_emtab = score_external_datasets(best_estimator,params)
+        results['best_epoch']['uams_metric'] = cindex_uams
+        results['best_epoch']['hovon_metric'] = cindex_hovon
+        results['best_epoch']['emtab_metric'] = cindex_emtab
+
+
+    os.makedirs(os.path.dirname(params.resultsprefix),exist_ok=True)
 
     # save results
     with open(f'{params.resultsprefix}.json', 'w') as f:
         json_dump(results, f, indent=4)
 
     # save model state dict
-    grid_search.best_estimator_.model.save(f'{params.resultsprefix}.pth')
+    # either estimator class or model class should implement `save`
+    grid_search.best_estimator_.save(f'{params.resultsprefix}.pth')
 
     return 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='Tune hyperparameters of VAE model using scikit-learn GridSearchCV. For adjusting hyperparameters, modify params.py and param_grid.py')
-    parser.add_argument('-e','--endpoint', type=str, choices=['pfs', 'os'], default='pfs', help='Survival endpoint (pfs or os)')
-    parser.add_argument('-a', '--architecture', type=str, choices=['VAE','Deepsurv'], default='VAE', help='Choice of model architecture. In-house VAE or comparator Deepsurv.')
+    parser.add_argument('-m','--model_name', type=str, help='An experiment name for the model')
+    parser.add_argument('-e','--endpoint', type=str, choices=['pfs', 'os','both'], help='Survival endpoint (pfs or os)')
+    parser.add_argument('-a', '--architecture', type=str, choices=['VAE','Deepsurv'], help='Choice of model architecture. In-house VAE or comparator Deepsurv.')
     parser.add_argument('-f', '--fulldata', action='store_true', help='Flag to indicate using train + validation data')
     args = parser.parse_args()
     _pbs_array_id = int(os.getenv('PBS_ARRAY_INDEX', "-1"))
     pbs_shuffle=_pbs_array_id%10
     pbs_fold=_pbs_array_id//10
-    main(args.endpoint, pbs_shuffle, pbs_fold, args.architecture, args.fulldata)
+    if args.endpoint=="both":
+        # useful for training the train-valid splits
+        # because scheduler has a limit of 99 jobs
+        main(args.model_name, 'os', pbs_shuffle, pbs_fold, args.architecture, args.fulldata)
+        main(args.model_name, 'pfs', pbs_shuffle, pbs_fold, args.architecture, args.fulldata)
+    else:
+        main(args.model_name, args.endpoint, pbs_shuffle, pbs_fold, args.architecture, args.fulldata)
