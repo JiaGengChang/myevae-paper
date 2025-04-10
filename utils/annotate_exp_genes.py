802f347302020e008df112ae777ab0f66b65798d
jiageng <jiageng1997@gmail.com>
2025-04-10 22:04:42 +0800

    rename exp_genes to all_exp_genes

--- utils/annotate_exp_genes.py
+++ utils/annotate_exp_genes.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-def annotate_exp_genes(train_dataframe:pd.DataFrame, params:dict):
+# update params with all RNA-Seq genes used in dataset
+# these are all the genes that can possibly be used for training
+def annotate_exp_genes(train_dataframe:pd.DataFrame, params:dict, fieldname:str='all_exp_genes'):
     exp_features = train_dataframe.filter(regex='Feature_exp').columns.str.extract('.*(ENSG.*)').iloc[:,0].tolist()
     assert len(exp_features)>0
-    params.exp_genes = exp_features
+    setattr(params, fieldname, exp_features)
     return params
