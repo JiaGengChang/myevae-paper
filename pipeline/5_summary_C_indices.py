f698b3d31ef69c887a33340785b2a2369b36680a
jiageng <jiageng1997@gmail.com>
2025-04-11 20:46:18 +0800

    refactor

--- pipeline/5_summary_C_indices.py
+++ pipeline/5_summary_C_indices.py
@@ -5,11 +5,17 @@ import numpy as np
 from dotenv import load_dotenv
 assert load_dotenv('.env') or load_dotenv('../.env')
 
-def summary_statistics_single_model(model_name):
+def summary_statistics_single_model(model_path):
+    """
+    @model_path: full path to the model results folder
+    the model folder will have 50 json files for OS and 50 for PFS
+    in the form shuffleS_foldF.json, S=0-9, F=0-4
+    """
     model = {}
+    # output directory folder is VAE_models or deepsurv_models
     for endpoint in ['os', 'pfs']:
         model[endpoint] = {}
-        jsonfiles=glob(f"{os.environ.get('OUTPUTDIR')}/{model_name}/{endpoint}_shuffle*_fold*.json")    
+        jsonfiles=glob(f"{model_path}/{endpoint}_*.json") # shuffle*_fold* or full 
         valid_metric = []
         uams_metric = []
         hovon_metric = []
@@ -43,16 +49,11 @@ def summary_statistics_single_model(model_name):
     
     return model
 
-def summary_statistics_many_models(model_names):
-    scores = {model_name: summary_statistics_single_model(model_name) for model_name in model_names}
-    return scores
-
 if __name__ == "__main__":
-    model_paths = glob(f"{os.environ.get('OUTPUTDIR')}/*")
-    model_names = [os.path.basename(path) for path in model_paths]
+    model_paths = glob(f"{os.environ.get('OUTPUTDIR')}/*_models/*")
 
-    scores = summary_statistics_many_models(model_names)
+    scores = {model_path: summary_statistics_single_model(model_path) for model_path in model_paths}
     filtered_scores = {k: v for k, v in sorted(scores.items()) if v is not None}
 
-    with open(f'{os.environ.get("OUTPUTDIR")}/model_scores.json', 'w') as f:
+    with open(f'{os.environ.get("PROJECTDIR")}/model_scores.json', 'w') as f:
         json.dump(filtered_scores, f, indent=4)
