99cc69769f8d700095a4f3e1200924f3436eb1cc
jiageng <jiageng1997@gmail.com>
2026-09-18 22:12:34 +0800

    merge from main

--- pipeline/3_gridsearchcv.py
+++ pipeline/3_gridsearchcv.py
@@ -38,7 +38,7 @@ def main(model_name:str='default',
          subset:bool=False) -> None:
     
     if architecture=='VAE':
-        from utils.params import VAEParams as Params
+        from utils.params import VAEParams
         from modules_vae.param_grid import param_grid
         params = VAEParams(model_name=model_name, endpoint=endpoint, shuffle=shuffle, fold=fold, fulldata=fulldata, subset=subset)
         param_grid = param_grid_vae
