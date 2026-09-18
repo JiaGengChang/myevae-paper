e60da8e535e607997b41fca2d6c93dbde315caa7
jiageng <jiageng1997@gmail.com>
2026-09-19 03:53:35 +0800

    add gridsearch CV PBS job script

---
+++ job_gridsearch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#PBS -N vae_zero_impute_mask_aware
+#PBS -P 11004309
+#PBS -j oe
+#PBS -o /home/users/nus/e1083772/cancer-survival-ml/.pbs/3_gridsearchcv/vae_zero_impute_mask_aware/
+#PBS -q normal
+#PBS -l select=1:ncpus=10:mem=32G
+#PBS -l walltime=23:59:59
+#PBS -J 0-49
+
+set -e
+
+module load python/3.12.1-gcc11
+
+source /home/users/nus/e1083772/python3.12_venv/bin/activate
+
+python /home/users/nus/e1083772/cancer-survival-ml/pipeline/3_gridsearchcv.py --endpoint both
