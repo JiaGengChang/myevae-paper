46d8517931f8ba633112e5c7c1b734d4204702d5
jiageng <jiageng1997@gmail.com>
2026-09-20 21:08:24 +0800

    minor update

--- job_gridsearch.sh
+++ job_gridsearch.sh
@@ -6,7 +6,7 @@
 #PBS -o /home/users/nus/e1083772/cancer-survival-ml/.pbs/3_gridsearchcv/vae_zero_impute_naive/
 #PBS -q normal
 #PBS -l select=1:ncpus=10:mem=32G
-#PBS -l walltime=11:59:59
+#PBS -l walltime=02:00:01
 #PBS -J 0-49
 
 set -e
@@ -15,4 +15,4 @@ module load python/3.12.1-gcc11
 
 source /home/users/nus/e1083772/python3.12_venv/bin/activate
 
-python /home/users/nus/e1083772/cancer-survival-ml/pipeline/3_gridsearchcv.py --n-iter 100 --endpoint os
+python /home/users/nus/e1083772/cancer-survival-ml/pipeline/3_gridsearchcv.py --n-iter 100 --endpoint both
