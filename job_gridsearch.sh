6cd48fbbfd91b66535708f8eb51e290bd9bf9305
jiageng <jiageng1997@gmail.com>
2026-09-20 23:33:44 +0800

    update walltime

--- job_gridsearch.sh
+++ job_gridsearch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#PBS -N vae_drop_missing
+#PBS -N dropMissing_
 #PBS -P 11004309
 #PBS -j oe
 #PBS -o /home/users/nus/e1083772/cancer-survival-ml/.pbs/3_gridsearchcv/vae_drop_missing/
 #PBS -q normal
 #PBS -l select=1:ncpus=10:mem=32G
-#PBS -l walltime=11:59:59
+#PBS -l walltime=04:00:01
 #PBS -J 0-49
 
 set -e
@@ -15,4 +15,4 @@ module load python/3.12.1-gcc11
 
 source /home/users/nus/e1083772/python3.12_venv/bin/activate
 
-python /home/users/nus/e1083772/cancer-survival-ml/pipeline/3_gridsearchcv.py --endpoint both
+python /home/users/nus/e1083772/cancer-survival-ml/pipeline/3_gridsearchcv.py --n-iter 100 --endpoint both
