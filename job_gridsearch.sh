2004d68deba7009b932e84d8f89fafbcb2835f63
jiageng <jiageng1997@gmail.com>
2026-09-20 23:08:12 +0800

    abbrv job name

--- job_gridsearch.sh
+++ job_gridsearch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#PBS -N vae_zero_impute_mask_aware
+#PBS -N mask0imp_
 #PBS -P 11004309
 #PBS -j oe
 #PBS -o /home/users/nus/e1083772/cancer-survival-ml/.pbs/3_gridsearchcv/vae_zero_impute_mask_aware/
 #PBS -q normal
 #PBS -l select=1:ncpus=10:mem=32G
-#PBS -l walltime=11:59:59
+#PBS -l walltime=02:00:01
 #PBS -J 0-49
 
 set -e
