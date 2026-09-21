8e2f40b904a6d663a19175ca8e6a74e5c60abf50
jiageng <jiageng1997@gmail.com>
2026-09-22 02:23:58 +0800

    round 2 param grid for exp-only VAE

--- modules_vae/param_grid.py
+++ modules_vae/param_grid.py
@@ -1,9 +1,9 @@
 from torch.nn import LeakyReLU, ReLU, Sigmoid, Tanh
 
 param_grid = {
-    'z_dim': [8, 16, 32, 64],
-    'lr': [5e-4, 1e-4, 5e-5],
-    'batch_size': [64, 128, 256],
+    'z_dim': [16, 32, 48],
+    'lr': [1e-4, 5e-5, 1e-5],
+    'batch_size': [32, 48, 64],
     'input_types': [['exp']],
     'input_types_subtask': [['clin']],
     'layer_dims': [[[64, 16]], [[128, 32]], [[256, 64]]],
