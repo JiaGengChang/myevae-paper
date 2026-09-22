d94cb565d945132df5fa4ea67c400abbbf5f7692
jiageng <jiageng1997@gmail.com>
2026-09-23 00:58:23 +0800

    vae sbs round 1

--- modules_vae/param_grid.py
+++ modules_vae/param_grid.py
@@ -1,16 +1,15 @@
 from torch.nn import LeakyReLU, ReLU, Sigmoid, Tanh
 
 param_grid = {
-    'z_dim': [64,128],
-    'lr': [1e-4, 2e-4, 3e-4], 
-    'batch_size': [64, 96, 128],
-    'input_types': [['exp','cna', 'gistic', 'fish', 'sbs', 'ig']],
+    'z_dim': [8, 16, 32],
+    'lr': [5e-4, 1e-4, 5e-5], 
+    'batch_size': [128, 256, 512],
+    'input_types': [['exp','cna', 'gistic', 'fish', 'sbs']],
     'input_types_subtask': [['clin']],
-    'layer_dims': [
-        [[128, 32], [64, 16], [16, 8], [16, 4], [4], [2]],
-        [[256, 64], [128, 32], [32, 8], [16, 4], [4], [2]],
-    ],
-    'layer_dims_subtask': [[4,1], [2,1], [6,1]],
+    'layer_dims': [[[64, 16], [16, 8], [16, 4], [16, 4], [4]], 
+                   [[128, 32], [64, 16], [16, 8], [16, 4], [4]], 
+                   [[256, 64], [128, 32], [32, 8], [16, 4], [4]]],
+    'layer_dims_subtask' : [[4,1], [8,1], [16,1]],
     'kl_weight': [1],
     'activation': [LeakyReLU()],
     'subtask_activation': [Tanh()],
