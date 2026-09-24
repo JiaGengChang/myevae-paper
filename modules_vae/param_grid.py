43f3475a16a3b81ee72efc8981e23b1f266d3cb8
jiageng <jiageng1997@gmail.com>
2026-09-24 21:00:49 +0800

    param grid for exp-cna

--- modules_vae/param_grid.py
+++ modules_vae/param_grid.py
@@ -1,14 +1,12 @@
 from torch.nn import LeakyReLU, ReLU, Sigmoid, Tanh
 
 param_grid = {
-    'z_dim': [8, 16, 32, 64],
+    'z_dim': [8, 16, 32],
     'lr': [5e-4, 1e-4, 5e-5], 
     'batch_size': [128, 256, 512],
-    'input_types': [['exp','cna', 'gistic', 'fish', 'sbs', 'ig']],
+    'input_types': [['exp','cna']],
     'input_types_subtask': [['clin']],
-    'layer_dims': [[[64, 16], [16, 8], [16, 4], [16, 4], [4], [2]], 
-                   [[128, 32], [64, 16], [16, 8], [16, 4], [4], [2]], 
-                   [[256, 64], [128, 32], [32, 8], [16, 4], [4], [2]]],
+    'layer_dims': [[[64, 16], [16, 8]], [[128, 32], [64, 16]], [[256, 64], [128, 32]]],
     'layer_dims_subtask' : [[4,1], [8,1], [16,1]],
     'kl_weight': [1],
     'activation': [LeakyReLU()],
