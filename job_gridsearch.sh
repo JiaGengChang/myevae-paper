6184b9f9333ff06bd7178c8596476197b629642c
jiageng <jiageng1997@gmail.com>
2026-09-25 22:42:55 +0800

    exp only model tuning

--- job_gridsearch.sh
+++ job_gridsearch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -N partialMask_exp_cna_gistic
+#PBS -N partialMask_expOnly
 #PBS -P 11004309
 #PBS -j oe
 #PBS -o /home/users/nus/e1083772/cancer-survival-ml/.pbs/3_gridsearchcv/vae_partial_mask/
