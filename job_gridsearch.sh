b86be99fbc5f2cb67fd92ebf573df198bbf5fbf8
jiageng <jiageng1997@gmail.com>
2026-09-26 16:02:56 +0800

    rerun of exp model

--- job_gridsearch.sh
+++ job_gridsearch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#PBS -N partialMask_full
+#PBS -N partialMask_exp_pfs
 #PBS -P 11004309
 #PBS -j oe
 #PBS -o /home/users/nus/e1083772/cancer-survival-ml/.pbs/3_gridsearchcv/vae_partial_mask/
 #PBS -q normal
-#PBS -l select=1:ncpus=5:mem=32G
+#PBS -l select=1:ncpus=2:mem=32G
 #PBS -l walltime=04:00:00
-#PBS -J 0-49
+#PBS -J 47-49
 
 set -e
 
@@ -15,4 +15,4 @@ module load python/3.12.1-gcc11
 
 source /home/users/nus/e1083772/python3.12_venv/bin/activate
 
-python /home/users/nus/e1083772/cancer-survival-ml/pipeline/3_gridsearchcv.py --n-iter 100 --endpoint both
+python /home/users/nus/e1083772/cancer-survival-ml/pipeline/3_gridsearchcv.py --n-iter 100 --endpoint pfs
