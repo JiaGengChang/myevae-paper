98851bd62b6d89833f609f5416c57cfb56eac9a2
jiageng <jiageng1997@gmail.com>
2026-09-26 18:38:30 +0800

    update model json path

--- pipeline/5_summary.py
+++ pipeline/5_summary.py
@@ -57,7 +57,7 @@ def summary_statistics_single_model(model_path):
     return model
 
 if __name__ == "__main__":
-    model_paths = glob(f"{os.environ.get('OUTPUTDIR')}/*/*")
+    model_paths = glob(f"{os.environ.get('OUTPUTDIR')}/robust_vae_models/vae_zero_impute_mask_aware/*")
 
     scores = {model_path: summary_statistics_single_model(model_path) for model_path in model_paths}
     filtered_scores = {k: v for k, v in sorted(scores.items()) if v is not None}
